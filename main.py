56c42419d2856616bb2eff8136b48f8018a8477f
erhuve <rlhive@hotmail.com>
2020-11-02 23:50:57 -0500

    First draft

---
+++ main.py
@@ -0,0 +1,19 @@
+import csv
+import pandas as pd
+import plotly.express as px
+import plotly.io as pio
+
+newfile = open('cleaned.csv', 'w')
+writer = csv.writer(newfile)
+writer.writerow(['LEAID','STATE','ZIP','OP'])
+
+df = pd.read_csv('data.csv')
+
+for index, row in df.iterrows():
+    writer.writerow([row['LEAID'],row['STATENAME'],row['MZIP'],row['OPERATIONAL_SCHOOLS']])
+newfile.close()
+
+# df = pd.read_csv('cleaned.csv')
+# leas = df.groupby('STATE')
+
+# print(leas)
